02b79a3e7645a9519c9f65e1736fde7cbcbc5303
Salman <salman666online@gmail.com>
2024-07-11 18:39:37 +0500

    updated http proxy settings

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -1,11 +1,12 @@
 // This file is used to configure the proxy for the rpc (to make calls from https to http)
+import { Application } from 'express';
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function(app: Application) {
   app.use(
     '/rpc',
     createProxyMiddleware({
-      target: 'http://62.171.133.46:38000',
+      target: process.env.REACT_APP_RPC_URL,
       changeOrigin: true,
       pathRewrite: {
         '^/rpc': '', // remove /rpc from the URL path before forwarding
