d023058f779553664c30e17950d22350a18db45b
MSalman6 <salman666online@gmail.com>
2024-08-20 18:23:22 +0500

    fixed ecosystem parameter change and create proposal issues

--- src/components/DaoPhaseBanner/index.tsx
+++ src/components/DaoPhaseBanner/index.tsx
@@ -23,16 +23,18 @@ const DaoPhaseBanner: React.FC<DaoProps> = () => {
 
   return (
     <div className={styles.daoPhaseBanner}>
-        {daoContext.daoPhase?.phase === "1" && <div></div>}
-        <h4>
-            {daoContext.daoPhase?.phase === "0" ? "Proposal" : "Voting"} Phase {daoContext.daoPhaseCount}
-        </h4>
-        <p>{daoContext.phaseEndTimer} till the end</p>
-        {daoContext.daoPhase?.phase === "0" && unfinalizedProposalsExist ? (
-          <FinalizeProposalsWarn buttonText="Create Proposal" />
-        ) : (
-          <button className="primaryBtn" onClick={() => {startTransition(() => {navigate("/dao/create")})}}>Create Proposal</button>
-        )
+      {daoContext.daoPhase?.phase === "1" && <div></div>}
+      <h4>
+        {daoContext.daoPhase?.phase === "0" ? "Proposal" : "Voting"} Phase {daoContext.daoPhaseCount}
+      </h4>
+      <p>{daoContext.phaseEndTimer} till the end</p>
+      {daoContext.daoPhase?.phase === "1" ? (
+        <p></p>
+      ) : unfinalizedProposalsExist ? (
+        <FinalizeProposalsWarn buttonText="Create Proposal" />
+      ) : daoContext.daoPhase?.phase === "0" && (
+        <button className="primaryBtn" onClick={() => { startTransition(() => { navigate("/dao/create") }) }}>Create Proposal</button>
+      )
       }
     </div>
   );
