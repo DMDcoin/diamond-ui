9d7826ecc47b08f3e946d604613fe6ee84a3d431
MSalman6 <salman666online@gmail.com>
2024-11-13 16:30:47 +0500

    fixed status sorting

--- src/components/ValidatorsTable/index.tsx
+++ src/components/ValidatorsTable/index.tsx
@@ -149,7 +149,7 @@ const ValidatorsTable: React.FC<ValidatorsTableProps> = ({ itemsPerPage = 100 })
                     <thead>
                         <tr>
                             <th></th>
-                            <th className={getClassNamesFor('isCurrentValidator')} onClick={() => requestSort('isCurrentValidator')}>
+                            <th className={getClassNamesFor('isActive')} onClick={() => requestSort('isActive')}>
                                 Status
                                 <Tooltip text="Active candidate is part of the active set; Valid - is not part of the active set, but can be elected; Invalid - a candidate, who is inactive for some period of time" />
                                 <FontAwesomeIcon icon={faSort} size="xs" />
