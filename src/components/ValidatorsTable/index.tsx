d94259b75e879c84afd96c351e981a9742fbcb50
MSalman6 <salman666online@gmail.com>
2025-01-08 02:50:30 +0500

    Functionality to customize (add/remove and sort columns and their values)

--- src/components/ValidatorsTable/index.tsx
+++ src/components/ValidatorsTable/index.tsx
@@ -15,6 +15,19 @@ import copy from "copy-to-clipboard";
 import { toast } from "react-toastify";
 import ColumnsFilterModal from "./ColumnsFilter";
 
+let tableFieldsDefault = [
+    { key: "jazzIcon", label: "", sortAble: false, updateAble: false, hide: false },
+    { key: "isActive", label: "Status", sortAble: true, updateAble: true, hide: false },
+    { key: "stakingAddress", label: "Wallet", sortAble: false, updateAble: true, hide: false },
+    { key: "totalStake", label: "Total Stake", sortAble: true, updateAble: true, hide: false },
+    { key: "votingPower", label: "Voting Power", sortAble: true, updateAble: true, hide: false },
+    { key: "score", label: "Score", sortAble: true, updateAble: true, hide: false },
+    { key: "connectivityReport", label: "CR", sortAble: true, updateAble: true, hide: false },
+    { key: "myStake", label: "My Stake", sortAble: true, updateAble: false, hide: false },
+    { key: "stakeBtn", label: "", sortAble: false, updateAble: false, hide: false },
+    { key: "unstakeClaimBtn", label: "", sortAble: false, updateAble: false, hide: false },
+];
+
 interface ValidatorsTableProps {
     itemsPerPage?: number;
 }
@@ -31,12 +44,29 @@ const ValidatorsTable: React.FC<ValidatorsTableProps> = ({ itemsPerPage = 100 })
 
     const [sortConfig, setSortConfig] = useState<{ key: string, direction: string } | null>(null);
 
+    const [tableFields, setTableFields] = useState(tableFieldsDefault);
+
     useEffect(() => {
         // If a filter is passed via state, apply it
         if (location.state?.filter) {
             setFilter(location.state.filter);
         }
     }, [location.state]);
+
+    // Load tableFieldsDefault from local storage if it exists, otherwise use the default
+    useEffect(() => {
+        const storedTableFields = localStorage.getItem('validatorFieldsData');
+        if (storedTableFields) {
+            setTableFields(JSON.parse(storedTableFields));
+        } else {
+            localStorage.setItem('validatorFieldsData', JSON.stringify(tableFieldsDefault));
+        }
+    }, []);
+
+    // Update local storage whenever tableFields changes
+    useEffect(() => {
+        localStorage.setItem('validatorFieldsData', JSON.stringify(tableFields));
+    }, [tableFields]);
 
     // Handle filter change
     const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -55,7 +85,7 @@ const ValidatorsTable: React.FC<ValidatorsTableProps> = ({ itemsPerPage = 100 })
         copy(data);
         toast.success(msg);
     };
-
+    
     // Apply filters and search
     let poolsCopy = [...pools];
 
@@ -81,7 +111,7 @@ const ValidatorsTable: React.FC<ValidatorsTableProps> = ({ itemsPerPage = 100 })
         poolsCopy.sort((a: any, b: any) => {
             let keyA, keyB;
     
-            if (sortConfig.key === 'myStake' || sortConfig.key === 'score') {
+            if (sortConfig.key === 'myStake' || sortConfig.key === 'score' || sortConfig.key === 'connectivityReport') {
                 keyA = parseFloat(a[sortConfig.key] || '0');
                 keyB = parseFloat(b[sortConfig.key] || '0');
             } else {
@@ -138,6 +168,141 @@ const ValidatorsTable: React.FC<ValidatorsTableProps> = ({ itemsPerPage = 100 })
         return pageNumbers;
     };
 
+    const renderHeaders = () => {
+        return (
+            <thead>
+                <tr>
+                    {tableFields.filter(field => !field.hide).map((column, index) => {
+                        if (column.key === 'myStake' && !userWallet.myAddr) {
+                            return <th key={index}></th>; // Skip rendering "My Stake" column if userWallet.myAddr is not present
+                        }
+                        return (
+                            <th
+                                key={index}
+                                className={getClassNamesFor(column.key)}
+                                onClick={() => column.sortAble && requestSort(column.key)}
+                            >
+                                {column.label}
+                                {column.sortAble && column.key && column.key !== 'stakingAddress' && column.key !== 'jazzIcon' && column.key != 'stakeBtn' && column.key != 'unstakeClaimBtn' && (
+                                    <>
+                                        {column.key !== 'myStake' && <Tooltip text={getTooltipText(column.key)} />}
+                                        <FontAwesomeIcon icon={faSort} size="xs" />
+                                    </>
+                                )}
+                            </th>
+                        );
+                    })}
+                </tr>
+            </thead>
+        );
+    };
+    
+    const renderRows = (currentItems: any[]) => {
+        return currentItems.map((pool, index) => (
+            <tr onClick={() => navigate(`/staking/details/${pool.stakingAddress}`)} className={styles.tableBodyRow} key={index}>
+                {tableFields.filter(field => !field.hide).map((column, colIndex) => {
+                    if (column.key === 'jazzIcon') {
+                        return (
+                            <td key={colIndex}>
+                                <Jazzicon diameter={40} seed={jsNumberForAddress(pool.stakingAddress)} />
+                            </td>
+                        );
+                    } else if (column.key === 'isActive') {
+                        return (
+                            <td key={colIndex} className={pool?.isActive || (pool.isToBeElected || pool.isPendingValidator) ? styles.poolActive : styles.poolBanned}>
+                                {typeof pool.isActive === 'boolean'
+                                    ? pool.isActive ? <strong>Active</strong> : (pool.isToBeElected || pool.isPendingValidator) ? "Valid" : "Invalid"
+                                    : (<div className={styles.loader}></div>)}
+                            </td>
+                        );
+                    } else if (column.key === 'stakingAddress') {
+                        return (
+                            <td key={colIndex} className={styles.addressesContainer + " " + styles.walletColumn}>
+                                {pool.stakingAddress ? (
+                                    <>
+                                        <div onClick={(e) => copyData(e, pool.miningAddress, "Copied staking address")}>{pool.stakingAddress}</div>
+                                        {pool.miningAddress && (
+                                            <div onClick={(e) => copyData(e, pool.miningAddress, "Copied mining address")}>{pool.miningAddress}</div>
+                                        )}
+                                    </>
+                                ) : (
+                                    <div className={styles.loader}></div>
+                                )}
+                            </td>
+                        );
+                    } else if (column.key === 'totalStake') {
+                        return (
+                            <td key={colIndex}>
+                                {BigNumber(pool.totalStake ?? 0) ? BigNumber(BigNumber(pool.totalStake ?? 0)).dividedBy(10**18).toFixed(4) + " DMD" : (<div className={styles.loader}></div>)}
+                            </td>
+                        );
+                    } else if (column.key === 'votingPower') {
+                        return (
+                            <td key={colIndex}>
+                                {pool.votingPower && pool.votingPower.toString() !== 'NaN' && pool.votingPower.toString() !== 'Infinity'
+                                    ? `${pool.votingPower.toString()} %`
+                                    : <div className={styles.loader}></div>}
+                            </td>
+                        );
+                    } else if (column.key === 'score') {
+                        return (
+                            <td key={colIndex}>{pool.score !== undefined && pool.score !== null ? pool.score : (<div className={styles.loader}></div>)}</td>
+                        );
+                    } else if (column.key === 'connectivityReport') {
+                        return (
+                            <td key={colIndex} style={{ color: pool.isFaultyValidator ? 'red' : (Number(pool.connectivityReport) > 0 ? 'orange' : 'inherit'), fontWeight: pool.isFaultyValidator ? 'bold' : 'normal' }}>
+                                {pool.connectivityReport !== undefined && pool.connectivityReport !== null ? pool.connectivityReport : (<div className={styles.loader}></div>)}
+                            </td>
+                        );
+                    } else if (column.key === 'myStake' && userWallet.myAddr) {
+                        return (
+                            <td key={colIndex}>{userWallet.myAddr && BigNumber(pool.myStake) ? BigNumber(pool.myStake).dividedBy(10**18).toFixed(0) : (<div className={styles.loader}></div>)} DMD</td>
+                        );
+                    } else if (column.key === 'stakeBtn' && userWallet.myAddr) {
+                        return (
+                            <td key={colIndex}>
+                                {(pool.isActive || pool.isToBeElected || pool.isPendingValidator) && BigNumber(pool.totalStake ?? 0).isLessThan(BigNumber(50000).multipliedBy(10**18)) && (
+                                    <StakeModal buttonText="Stake" pool={pool} />
+                                )}
+                            </td>
+                        );
+                    } else if (column.key === 'unstakeClaimBtn' && userWallet.myAddr) {
+                        return (
+                            <td key={colIndex}>
+                                {BigNumber(pool.orderedWithdrawAmount).isGreaterThan(0) && BigNumber(pool.orderedWithdrawUnlockEpoch).isLessThanOrEqualTo(stakingEpoch) ? (
+                                    <button className="primaryBtn" onClick={(e) => {e.stopPropagation(); claimOrderedUnstake(pool)}}>Claim</button>
+                                ) : (
+                                    BigNumber(pool.myStake).isGreaterThan(0) && (
+                                        <UnstakeModal buttonText="Unstake" pool={pool} />
+                                    )
+                                )}
+                            </td>
+                        );
+                    } else {
+                        return <td key={colIndex}></td>;
+                    }
+                })}
+            </tr>
+        ));
+    };    
+    
+    const getTooltipText = (key: string) => {
+        switch (key) {
+            case 'isActive':
+                return "Active candidate is part of the active set; Valid - is not part of the active set, but can be elected; Invalid - a candidate who is flagged unavailable on the blockchain or has not enough stake";
+            case 'totalStake':
+                return "Total delegated DMD (self-staked DMD + delegates' stake)";
+            case 'votingPower':
+                return "Value that approximates a node’s influence in the DAO participation";
+            case 'score':
+                return "Combined score value, based on the results of generating the shared key, the stability of the validator connection and misbehaviour reports from other validators";
+            case 'connectivityReport':
+                return "Connectivity report value, based on how many other active validators did report bad connectivity towards that node.";
+            default:
+                return "";
+        }
+    };    
+
     return (
         <div className={styles.sectionContainer + " sectionContainer"}>
             <Navigation start="/" toPage="/" />
@@ -181,13 +346,14 @@ const ValidatorsTable: React.FC<ValidatorsTableProps> = ({ itemsPerPage = 100 })
                     <option value="stakedOn">Candidates I've staked on</option>
                 </select>
 
-                <ColumnsFilterModal buttonText="Columns" />
+                <ColumnsFilterModal buttonText="Customize" tableFields={tableFields} setTableFields={setTableFields} defaultFields={tableFieldsDefault} />
             </div>
 
             {/* Table */}
             <div className={styles.tableContainer}>
                 <table className={styles.styledTable}>
-                    <thead>
+                    {renderHeaders()}
+                    {/* <thead>
                         <tr>
                             <th></th>
                             <th className={getClassNamesFor('isActive')} onClick={() => requestSort('isActive')}>
@@ -195,7 +361,7 @@ const ValidatorsTable: React.FC<ValidatorsTableProps> = ({ itemsPerPage = 100 })
                                 <Tooltip text="Active candidate is part of the active set; Valid - is not part of the active set, but can be elected; Invalid - a candidate who is flagged unavailable on the blockchain or has not enough stake" />
                                 <FontAwesomeIcon icon={faSort} size="xs" />
                             </th>
-                            <th className={getClassNamesFor('stakingAddress')}>
+                            <th className={getClassNamesFor('stakingAddress') + " " + styles.walletColumnHeader}>
                                 Wallet
                             </th>
                             <th className={getClassNamesFor('totalStake')} onClick={() => requestSort('totalStake')}>
@@ -229,9 +395,10 @@ const ValidatorsTable: React.FC<ValidatorsTableProps> = ({ itemsPerPage = 100 })
                             <th></th>
                             <th></th>
                         </tr>
-                    </thead>
+                    </thead> */}
                     <tbody>
-                        {currentItems.length <= 0 ? (
+                        {renderRows(currentItems)}
+                        {/* {currentItems.length <= 0 ? (
                             <tr>
                                 <td colSpan={9} className={styles.noData}>
                                     No validators found
@@ -247,7 +414,7 @@ const ValidatorsTable: React.FC<ValidatorsTableProps> = ({ itemsPerPage = 100 })
                                         ? pool.isActive ? <strong>Active</strong> : (pool.isToBeElected || pool.isPendingValidator) ? "Valid" : "Invalid"
                                         : (<div className={styles.loader}></div>)}
                                 </td>
-                                <td className={styles.addressesContainer}>
+                                <td className={styles.addressesContainer + " " + styles.walletColumn}>
                                     {pool.stakingAddress ? (
                                         <>
                                             <div onClick={(e) => copyData(e, pool.miningAddress, "Copied staking address")}>{pool.stakingAddress}</div>
@@ -300,7 +467,7 @@ const ValidatorsTable: React.FC<ValidatorsTableProps> = ({ itemsPerPage = 100 })
                                 }
                             </tr>
                         ))
-                        }
+                        } */}
                     </tbody>
                 </table>
             </div>
