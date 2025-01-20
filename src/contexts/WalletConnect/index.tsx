4ad05e2c07615776b34e38a01a3efc2b3e03cc18
MSalman6 <salman666online@gmail.com>
2025-01-20 23:53:36 +0500

    fixes for mobile screens

--- src/contexts/WalletConnect/index.tsx
+++ src/contexts/WalletConnect/index.tsx
@@ -19,6 +19,7 @@ const appKit = createWeb3Modal({
         "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", // metamask
         "163d2cf19babf05eb8962e9748f9ebe613ed52ebf9c8107c9a0f104bfcf161b3", // brave
         "fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa", // coinbase
+        // "a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393", // phantom
     ],
     themeMode: "light",
     themeVariables: {
